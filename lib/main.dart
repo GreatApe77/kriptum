713b6262cdfea3d0393a574990ba181ed08a2d6f
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-01-02 18:28:57 -0300

    create setup page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:kriptum/app.dart';
+
 void main(List<String> args) {
-  
+  runApp(const App());
 }
--- lib/ui/views/setup_page.dart
+++ lib/ui/views/setup_page.dart
@@ -5,6 +5,52 @@ class SetupPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('K R I P T U M',style: TextStyle(fontWeight: FontWeight.bold),),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              height: 24,
+            ),
+            const Text(
+              'Wallet setup',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 32
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            const Text(
+              'Import an existing wallet or create a new one',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                
+                fontSize: 16
+              ),
+            ),
+            Expanded(child: Container()),
+            OutlinedButton(
+
+              onPressed: () {}, child: const Text('Import using Secret Recovery Phrase')),
+            const SizedBox(height: 8,),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                
+              ),
+              onPressed: () {}, child: const Text('Create a new wallet')),
+            const SizedBox(height: 42,),
+          ],
+        ),
+      ),
+    );
   }
 }
