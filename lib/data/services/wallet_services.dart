75dd1bb546ac573968a14386a787ec946b4034cd
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-01-03 15:06:35 -0300

    add code to generate accounts

--- lib/data/services/wallet_services.dart
+++ lib/data/services/wallet_services.dart
@@ -1,51 +1,104 @@
 import 'package:bip39/bip39.dart' as bip39;
-import 'package:ed25519_hd_key/ed25519_hd_key.dart';
+
+import 'package:hd_wallet_kit/hd_wallet_kit.dart';
 import 'package:hex/hex.dart';
 import 'package:web3dart/web3dart.dart';
-
 class WalletServices {
   
-  // Generate a mnemonic phrase
+  // // Generate a mnemonic phrase
   String generateMnemonic() {
-    return bip39.generateMnemonic();
+    return Mnemonic.generate().join(' ');
   }
 
-  // Generate the private key for a specific derivation path
-  Future<String> getPrivateKey(String mnemonic, {int index = 0}) async {
+  // // Generate the private key for a specific derivation path
+  // Future<String> getPrivateKey(String mnemonic, {int index = 0}) async {
+  //   final seed = bip39.mnemonicToSeed(mnemonic);
+  //   final master = await ED25519_HD_KEY.getMasterKeyFromSeed(seed);
+    
+  //   // Derive the key using the BIP-44 path for Ethereum
+  //   final childKey = await ED25519_HD_KEY.derivePath(
+  //     "m/44'/60'/0'/0/$index",
+  //     seed,
+  //   );
+    
+  //   return HEX.encode(childKey.key);
+  // }
+
+  // // Generate an Ethereum address for a specific index
+  // Future<String> getAddress(String mnemonic, {int index = 0}) async {
+  //   // Get the private key for the given index
+  //   String privateKey = await getPrivateKey(mnemonic, index: index);
+
+  //   // Generate the public key and address
+  //   EthPrivateKey ethPrivateKey = EthPrivateKey.fromHex(privateKey);
+  //   EthereumAddress address = await ethPrivateKey.extractAddress();
+
+  //   return address.hex;
+  // }
+
+  // // Generate multiple addresses
+  // Future<List<String>> generateAddresses(String mnemonic, int count) async {
+  //   List<String> addresses = [];
+  //   for (int i = 0; i < count; i++) {
+  //     String address = await getAddress(mnemonic, index: i);
+  //     addresses.add(address);
+  //   }
+  //   return addresses;
+  // }
+  // Generate Ethereum addresses from a mnemonic
+  Future<List<String>> deriveAddresses(String mnemonic, int count) async {
+    if (!bip39.validateMnemonic(mnemonic)) {
+      throw ArgumentError('Invalid mnemonic phrase');
+    }
+
+    // Convert mnemonic to seed
     final seed = bip39.mnemonicToSeed(mnemonic);
-    final master = await ED25519_HD_KEY.getMasterKeyFromSeed(seed);
-    
-    // Derive the key using the BIP-44 path for Ethereum
-    final childKey = await ED25519_HD_KEY.derivePath(
-      "m/44'/60'/0'/0/$index",
-      seed,
-    );
-    
-    return HEX.encode(childKey.key);
-  }
 
-  // Generate an Ethereum address for a specific index
-  Future<String> getAddress(String mnemonic, {int index = 0}) async {
-    // Get the private key for the given index
-    String privateKey = await getPrivateKey(mnemonic, index: index);
+    // Initialize the HD wallet
+    final wallet = HDWallet.fromSeed(seed: seed);
 
-    // Generate the public key and address
-    EthPrivateKey ethPrivateKey = EthPrivateKey.fromHex(privateKey);
-    EthereumAddress address = await ethPrivateKey.extractAddress();
-
-    return address.hex;
-  }
-
-  // Generate multiple addresses
-  Future<List<String>> generateAddresses(String mnemonic, int count) async {
     List<String> addresses = [];
     for (int i = 0; i < count; i++) {
-      String address = await getAddress(mnemonic, index: i);
-      addresses.add(address);
+      // Derive the key for the given index
+      final key = wallet.deriveKey(
+        purpose: Purpose.BIP44,
+        coinType: 60, // Ethereum coin type
+        account: 0,
+        change: 0,
+        index: i,
+      );
+
+      // Generate the Ethereum address
+      final ethPrivateKey = EthPrivateKey.fromHex(HEX.encode(key.privKeyBytes!));
+      final address =  ethPrivateKey.address;
+
+      addresses.add(address.hex);
     }
+
     return addresses;
   }
 }
-void main(List<String> args) {
-  
+void main(List<String> args)async  {
+  // final service = WalletServices();
+  // print(service.generateMnemonic());
+  // final seed = Mnemonic.toSeed(mnemonic.split(' '));
+  // final wallet = HDWallet.fromSeed(seed: seed);
+  // final HDKey account0 = wallet.deriveKeyByPath(path: "m/44'/0'/0'");
+  // print(HEX.encode(account0.pubKey));
+  // final address0Key = wallet.deriveKey(
+  //     purpose: Purpose.BIP44, coinType: 0, account: 0, change: 0, index: 0);
+  // print(HEX.encode(address0Key.pubKeyHash));
+  // final ethPrivateKey = EthPrivateKey.fromHex(HEX.encode(address0Key.pubKey));
+  // print(ethPrivateKey.address);
+   final service = WalletServices();
+
+  // Generate a new mnemonic
+  final mnemonic = service.generateMnemonic();
+  //print('Mnemonic: $mnemonic');
+
+  // Derive and print the addresses
+  final addresses = await service.deriveAddresses(mnemonic, 20);
+  for (var i = 0; i < addresses.length; i++) {
+    print('Address $i: ${addresses[i]}');
+  }
 }
