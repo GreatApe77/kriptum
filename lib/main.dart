922a3a177c0d443b4a19dffebcc444a872a6f418
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-01-06 11:07:31 -0300

    add main entrypoint doc

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kriptum/app.dart';
 import 'package:kriptum/locator.dart';
-
+///Application entrypoint
 void main(List<String> args) {
   setup();
   runApp(const App());
