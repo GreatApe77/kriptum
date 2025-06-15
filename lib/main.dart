52df3edfd50fad30eecbdc0d67f7cd844db33eaa
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-06-15 09:31:36 -0300

    feat: initialize database in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:kriptum/config/di/injector.dart';
+import 'package:kriptum/infra/persistence/database/database.dart';
 import 'package:kriptum/ui/app.dart';
 
 Future<void> main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initInjector();
+  await injector.get<Database>().initialize();
   runApp(const App());
 }
