4f4b5e050bba7f758e312c4eabf62815270876ce
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-06-14 22:27:32 -0300

    feat: enhance SetupPage with wallet setup options and create MainTitleAppBarWidget

--- lib/ui/pages/setup/setup_page.dart
+++ lib/ui/pages/setup/setup_page.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
 
+import 'package:kriptum/ui/widgets/main_title_app_bar_widget.dart';
+
 class SetupPage extends StatelessWidget {
   const SetupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: MainTitleAppBarWidget(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              height: 24,
+            ),
+            const Text(
+              'Wallet setup',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 32
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            const Text(
+              'Import an existing wallet or create a new one',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                
+                fontSize: 16
+              ),
+            ),
+            Expanded(child: Container()),
+            OutlinedButton(
 
-      body: Center(
-        child: Text('Setup Page'),
+              onPressed: () => _navigateToImportWalletPage(context), child: const Text('Import using Secret Recovery Phrase')),
+            const SizedBox(height: 8,),
+            FilledButton(
+              onPressed: () => _navigateToCreateNewWalletPage(context), child: const Text('Create a new wallet')),
+            const SizedBox(height: 42,),
+          ],
+        ),
       ),
     );
+  }
+
+  void _navigateToCreateNewWalletPage(BuildContext context){
+     
+  }
+  void _navigateToImportWalletPage(BuildContext context){
+      
   }
 }
---
+++ lib/ui/widgets/main_title_app_bar_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MainTitleAppBarWidget extends StatelessWidget
+    implements PreferredSizeWidget {
+  const MainTitleAppBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: const Text(
+        'K R I P T U M',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
