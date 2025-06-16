2420a0fabccdf6740e19076c9fb15e3445d78a03
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-06-16 09:15:06 -0300

    create unit test

---
+++ test/mocks/mock_account_generator_service.dart
@@ -0,0 +1,5 @@
+import 'package:kriptum/domain/services/account_generator_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAccountGeneratorService extends Mock
+    implements AccountGeneratorService {}
---
+++ test/mocks/mock_accounts_repository.dart
@@ -0,0 +1,4 @@
+import 'package:kriptum/domain/repositories/accounts_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAccountsRepository extends Mock implements AccountsRepository {}
