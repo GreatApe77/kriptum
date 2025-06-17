5cae6764c6c5c78a6bf411c3e13bd0dc8c0266b8
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-06-17 09:50:47 -0300

    feat: add copy to clipboard functionality in HomeView with feedback dialog

---
+++ lib/shared/utils/copy_to_clipboard.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/services.dart';
+
+Future<void> copyToClipboard({
+  required String content,
+  Function(String content)? onCopied, 
+}) async {
+  await Clipboard.setData(ClipboardData(text: content));
+  if (onCopied != null) {
+    onCopied(content);
+  }
+}
--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kriptum/blocs/current_account/current_account_cubit.dart';
 import 'package:kriptum/config/di/injector.dart';
+import 'package:kriptum/shared/utils/copy_to_clipboard.dart';
+import 'package:kriptum/shared/utils/format_address.dart';
 import 'package:kriptum/ui/pages/home/widgets/account_viewer_btn.dart';
 import 'package:kriptum/ui/pages/home/widgets/accounts_modal.dart';
 
@@ -31,6 +33,24 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                copyToClipboard(
+                  content: context
+                      .read<CurrentAccountCubit>()
+                      .state
+                      .account!
+                      .address,
+                  onCopied: (text) => _onCopyToClipBoard(
+                    text,
+                    context,
+                  ),
+                );
+              },
+              icon: Icon(Icons.copy)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.qr_code)),
+        ],
         title: BlocBuilder<CurrentAccountCubit, CurrentAccountState>(
           builder: (context, state) {
             if (state.account == null) {
@@ -62,6 +82,43 @@ class HomeView extends StatelessWidget {
       showDragHandle: true,
       builder: (context) {
         return const AccountsModal();
+      },
+    );
+  }
+
+  void _onCopyToClipBoard(String text, BuildContext context) {
+    showDialog(
+      barrierColor: Colors.transparent,
+      context: context,
+      builder: (context) {
+        return Builder(builder: (context) {
+          return FutureBuilder(
+              future: Future.delayed(const Duration(seconds: 1))
+                  .then((value) => true),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  Navigator.of(context).pop();
+                }
+                return AlertDialog(
+                  title: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Icon(
+                        Icons.check_circle_rounded,
+                        size: 80,
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        'Public address ${formatAddress(text)} copied to clipboard',
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
+                );
+              });
+        });
       },
     );
   }
