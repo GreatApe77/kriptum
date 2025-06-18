6a96b9b1a9b9cbe690fcd92f61155003c28e9421
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-06-17 23:01:06 -0300

    feat: enhance MainBalanceViewer with visibility toggle and improved state handling

--- lib/ui/pages/home/widgets/main_balance_viewer.dart
+++ lib/ui/pages/home/widgets/main_balance_viewer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kriptum/blocs/native_balance/bloc/native_balance_bloc.dart';
+import 'package:kriptum/config/di/injector.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
 class MainBalanceViewer extends StatelessWidget {
@@ -9,22 +10,76 @@ class MainBalanceViewer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider<NativeBalanceBloc>(
-      create: (context) => NativeBalanceBloc()..add(NativeBalanceRequested()),
+      create: (context) => NativeBalanceBloc(
+        injector.get(),
+        injector.get()
+      )
+        ..add(
+          NativeBalanceRequested(),
+        )
+        ..add(
+          NativeBalanceVisibilityRequested(),
+        ),
       child: _MainBalanceViewer(),
     );
   }
 }
 
 class _MainBalanceViewer extends StatelessWidget {
-  const _MainBalanceViewer({super.key});
+  const _MainBalanceViewer();
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<NativeBalanceBloc, NativeBalanceState>(
       builder: (context, state) {
+        String content = '';
+        /* if (state.status == NativeBalanceStatus.error) {
+          content = state.errorMessage!;
+        }
+        if (state.status == NativeBalanceStatus.initial ||
+            state.status == NativeBalanceStatus.loading) {
+          content = 'Loading...';
+        }
+        if (state.status == NativeBalanceStatus.loaded) {
+          content = state.accountBalance!.toReadableString();
+        } */
+        switch (state.status) {
+          case NativeBalanceStatus.error:
+            content = state.errorMessage ?? 'An error occurred';
+            break;
+          case NativeBalanceStatus.initial:
+          case NativeBalanceStatus.loading:
+            content = 'Loading...';
+            break;
+          case NativeBalanceStatus.loaded:
+            content = state.accountBalance!.toReadableString();
+            break;
+        }
+        bool isVisible = state.isVisible;
+        if (!isVisible) {
+          content = '••••••••';
+        }
         return Skeletonizer(
-          enabled: state is NativeBalanceLoading,
-          child: Text('Test'),
+          enabled: state.status == NativeBalanceStatus.loading,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                child: Text(content),
+              ),
+              IconButton(
+                onPressed: () {
+                  context.read<NativeBalanceBloc>().add(
+                        ToggleNativeBalanceVisibility(isVisible: !isVisible),
+                      );
+                },
+                icon: Icon(
+                  isVisible ? Icons.visibility : Icons.visibility_off,
+                ),
+              ),
+            ],
+          ),
         );
       },
     );
