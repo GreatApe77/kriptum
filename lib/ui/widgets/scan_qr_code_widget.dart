c2f4bf3aee71a192d53d4ddd3343f8a2d2331d5c
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-06-24 19:02:56 -0300

    fix: qrcode scan stabiolity

--- lib/ui/widgets/scan_qr_code_widget.dart
+++ lib/ui/widgets/scan_qr_code_widget.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-class ScanQrCodeWidget extends StatelessWidget {
+class ScanQrCodeWidget extends StatefulWidget {
   const ScanQrCodeWidget({super.key});
 
+  @override
+  State<ScanQrCodeWidget> createState() => _ScanQrCodeWidgetState();
+}
+
+class _ScanQrCodeWidgetState extends State<ScanQrCodeWidget> {
+  bool _scanned = false;
   @override
   Widget build(BuildContext context) {
     return MobileScanner(
       onDetect: (result) {
-        final address = result.barcodes.first.rawValue;
-        //print('Scanned address: $address');
-        return Navigator.of(context).pop<String>(address);
+        if (!_scanned) {
+          _scanned = true;
+          final address = result.barcodes.first.rawValue;
+          Navigator.of(context).pop<String>(address);
+        }
       },
     );
   }
