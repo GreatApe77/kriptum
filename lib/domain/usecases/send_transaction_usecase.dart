1c92fa975c70b811ff8ff0876b9472836e5e970c
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-06-28 10:53:56 -0300

    feat: implement transaction service and use case for sending transactions

---
+++ lib/domain/usecases/send_transaction_usecase.dart
@@ -0,0 +1,63 @@
+import 'package:kriptum/domain/exceptions/domain_exception.dart';
+import 'package:kriptum/domain/repositories/accounts_repository.dart';
+import 'package:kriptum/domain/repositories/networks_repository.dart';
+import 'package:kriptum/domain/repositories/password_repository.dart';
+import 'package:kriptum/domain/services/transaction_service.dart';
+
+class SendTransactionUsecase {
+  final AccountsRepository _accountsRepository;
+  final PasswordRepository _passwordRepository;
+  final NetworksRepository _networksRepository;
+  final TransactionService _transactionService;
+  SendTransactionUsecase(
+    this._accountsRepository,
+    this._passwordRepository,
+    this._networksRepository,
+    this._transactionService,
+  );
+
+  Future<TransactionOutput> execute(SendTransactionUsecaseParams params) async {
+    final account = await _accountsRepository.getCurrentAccount();
+    if (account == null) {
+      throw DomainException('Account is null');
+    }
+    final currentNetwork = await _networksRepository.getCurrentNetwork();
+    final encryptionPassword = _passwordRepository.getPassword();
+    final txHash = await _transactionService.sendTransaction(
+      encryptedJsonAccount: account.encryptedJsonWallet,
+      password: encryptionPassword,
+      to: params.to,
+      amountInWei: params.amount,
+      rpcUrl: currentNetwork.rpcUrl,
+    );
+    String? transactionUrl;
+    if (currentNetwork.blockExplorerUrl != null) {
+      transactionUrl = '${currentNetwork.blockExplorerUrl}/tx/$txHash';
+    }
+    final txOutput = TransactionOutput(
+      transactionHash: txHash,
+      transactionUrlInBlockExplorer: transactionUrl,
+    );
+    return txOutput;
+  }
+}
+
+class SendTransactionUsecaseParams {
+  final String to;
+  final BigInt amount;
+
+  SendTransactionUsecaseParams({
+    required this.to,
+    required this.amount,
+  });
+}
+
+class TransactionOutput {
+  final String transactionHash;
+  final String? transactionUrlInBlockExplorer;
+
+  TransactionOutput({
+    required this.transactionHash,
+    this.transactionUrlInBlockExplorer,
+  });
+}
---
+++ lib/infra/services/transaction_service_impl.dart
@@ -0,0 +1,32 @@
+import 'package:kriptum/domain/services/transaction_service.dart';
+import 'package:http/http.dart' as http;
+import 'package:web3dart/web3dart.dart';
+
+class TransactionServiceImpl implements TransactionService {
+  final http.Client _httpClient;
+
+  TransactionServiceImpl(this._httpClient);
+
+  @override
+  Future<String> sendTransaction({
+    required String encryptedJsonAccount,
+    required String password,
+    required String to,
+    required BigInt amountInWei,
+    required String rpcUrl,
+  }) async {
+    final ethClient = Web3Client(rpcUrl, _httpClient);
+    final chainId = await ethClient.getChainId();
+    final account = Wallet.fromJson(encryptedJsonAccount, password);
+    final txHash = await ethClient.sendTransaction(
+      chainId: chainId.toInt(),
+      account.privateKey,
+      Transaction(
+        from: account.privateKey.address,
+        to: EthereumAddress.fromHex(to),
+        value: EtherAmount.inWei(amountInWei),
+      ),
+    );
+    return txHash;
+  }
+}
