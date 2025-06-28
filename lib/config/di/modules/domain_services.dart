f34f26ba3cf0fd1eee54367e8fa039df1167a975
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-06-28 10:56:41 -0300

    feat: register transaction service in dependency injection

--- lib/config/di/modules/domain_services.dart
+++ lib/config/di/modules/domain_services.dart
@@ -2,9 +2,11 @@ import 'package:kriptum/config/di/injector.dart';
 import 'package:kriptum/domain/services/account_decryption_with_password_service.dart';
 import 'package:kriptum/domain/services/account_generator_service.dart';
 import 'package:kriptum/domain/services/encryption_service.dart';
+import 'package:kriptum/domain/services/transaction_service.dart';
 import 'package:kriptum/infra/services/account_decryption_with_password_service_impl.dart';
 import 'package:kriptum/infra/services/account_generator_service_impl.dart';
 import 'package:kriptum/infra/services/encryption_service_impl.dart';
+import 'package:kriptum/infra/services/transaction_service_impl.dart';
 
 Future<void> registerDomainServices() async {
   injector.registerLazySingleton<EncryptionService>(
@@ -15,5 +17,10 @@ Future<void> registerDomainServices() async {
   );
   injector.registerLazySingleton<AccountDecryptionWithPasswordService>(
     () => AccountDecryptionWithPasswordServiceImpl(),
+  );
+  injector.registerLazySingleton<TransactionService>(
+    () => TransactionServiceImpl(
+      injector.get(),
+    ),
   );
 }
