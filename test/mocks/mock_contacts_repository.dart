1b31a3ad46bd409bbd204989ccbc839fed6c933f
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-07-08 07:47:07 -0300

    test: unit test add contact use case

---
+++ test/mocks/mock_contacts_repository.dart
@@ -0,0 +1,4 @@
+import 'package:kriptum/domain/repositories/contacts_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockContactsRepository extends Mock implements ContactsRepository {}
