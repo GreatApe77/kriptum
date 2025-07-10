7ef1ebca05208dbbcea544af2b81778ce76107c7
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-07-10 16:53:01 -0300

    feat: import erc20 token use case

--- lib/domain/repositories/erc20_token_repository.dart
+++ lib/domain/repositories/erc20_token_repository.dart
@@ -4,4 +4,6 @@ abstract interface class Erc20TokenRepository {
   Future<void> save(Erc20Token token);
   Future<List<Erc20Token>> getAllTokens();
   Future<void> delete(int tokenId);
+  Future<Erc20Token?> findByAddress(String contractAddress);
+  Future<List<Erc20Token>> getAllImportedTokensOfNetwork(int networkId);
 }
