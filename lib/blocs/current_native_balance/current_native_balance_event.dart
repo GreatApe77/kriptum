ce6dede4190afa526138f35d3e6c0f7a27aa699f
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-07-11 10:58:05 -0300

    feat: expose stream to be subscribed by blocs

--- lib/blocs/current_native_balance/current_native_balance_event.dart
+++ lib/blocs/current_native_balance/current_native_balance_event.dart
@@ -5,7 +5,11 @@ sealed class CurrentNativeBalanceEvent {}
 final class CurrentNativeBalanceRequested extends CurrentNativeBalanceEvent {}
 
 final class CurrentNativeBalanceVisibilityRequested extends CurrentNativeBalanceEvent {}
+final class _CurrentNativeBalanceVisibilityRefreshed extends CurrentNativeBalanceEvent{
+  final bool isVisible;
 
+  _CurrentNativeBalanceVisibilityRefreshed({required this.isVisible});
+}
 final class ToggleCurrentNativeBalanceVisibility extends CurrentNativeBalanceEvent {
   final bool isVisible;
 
