8ee6d477dde0d82260723e8c99bb6939aaa9c2bf
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-07-13 12:03:33 -0300

    feat: add migration for ERC20 tokens table and update database version

--- lib/infra/persistence/database/sqflite/run_migrations.dart
+++ lib/infra/persistence/database/sqflite/run_migrations.dart
@@ -1,10 +1,11 @@
 import 'package:kriptum/infra/persistence/database/sqflite/migrations/migrations_1.dart';
 import 'package:kriptum/infra/persistence/database/sqflite/migrations/migrations_2.dart';
 import 'package:kriptum/infra/persistence/database/sqflite/migrations/migrations_3.dart';
+import 'package:kriptum/infra/persistence/database/sqflite/migrations/migrations_4.dart';
 import 'package:sqflite/sqflite.dart';
 
 Future<void> runMigrations(Database db, int oldVersion, int newVersion) async {
-  final Map<int, Future<void> Function(Database)> migrations = {1: runMigration1, 2: runMigration2, 3: runMigration3};
+  final Map<int, Future<void> Function(Database)> migrations = {1: runMigration1, 2: runMigration2, 3: runMigration3,4:runMigration4};
 
   for (var version = oldVersion + 1; version <= newVersion; version++) {
     final migration = migrations[version];
---
+++ lib/infra/persistence/database/sqflite/tables/erc20_tokens_table.dart
@@ -0,0 +1,10 @@
+abstract class Erc20TokensTable {
+  static const String table = 'erc20_tokens';
+  static const String columnId = 'id';
+  static const String columnContractAddress = 'contract_address';
+  static const String columnName = 'name';
+  static const String columnSymbol = 'symbol';
+  static const String columnDecimals = 'decimals';
+  static const String columnLogoUrl = 'logo_url';
+  static const String columnNetworkId = 'network_id';
+}
