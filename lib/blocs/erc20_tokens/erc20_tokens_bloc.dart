0dc82b94caa6b61eceb5608fada54bb2cc289964
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-07-17 18:46:52 -0300

    feat: refactor Erc20TokensBloc to handle network changes and improve load request handling

--- lib/blocs/erc20_tokens/erc20_tokens_bloc.dart
+++ lib/blocs/erc20_tokens/erc20_tokens_bloc.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:kriptum/domain/exceptions/domain_exception.dart';
 import 'package:kriptum/domain/models/erc20_token.dart';
 import 'package:kriptum/domain/models/ether_amount.dart';
+import 'package:kriptum/domain/models/network.dart';
 import 'package:kriptum/domain/repositories/erc20_token_repository.dart';
 import 'package:kriptum/domain/repositories/networks_repository.dart';
 import 'package:kriptum/domain/usecases/get_erc20_balances_usecase.dart';
@@ -13,42 +16,56 @@ class Erc20TokensBloc extends Bloc<Erc20TokensEvent, Erc20TokensState> {
   final Erc20TokenRepository _erc20tokenRepository;
   final NetworksRepository _networksRepository;
   final GetErc20BalancesUsecase _getErc20BalancesUsecase;
+  late final StreamSubscription<Network>? _currentNetworkSubscription;
   Erc20TokensBloc(
     this._erc20tokenRepository,
     this._networksRepository,
     this._getErc20BalancesUsecase,
   ) : super(Erc20TokensState.initial()) {
-    on<Erc20TokensLoadRequested>((event, emit) async {
-      try {
-        emit(state.copyWith(status: Erc20TokensStatus.loading));
-        final network = await _networksRepository.getCurrentNetwork();
-        final tokens = await _erc20tokenRepository.getAllImportedTokensOfNetwork(network.id!);
-        final result = await _getErc20BalancesUsecase.execute();
-        final tokensWithBalances = tokens.map(
-          (e) {
-            return Erc20TokenWithBalance(
-              balance: result.balanceOf[e.address] ?? EtherAmount.fromString(value: '0'),
-              token: e,
-            );
-          },
-        ).toList();
-        emit(
-          state.copyWith(
-            status: Erc20TokensStatus.loaded,
-            tokens: tokensWithBalances,
-          ),
-        );
-      } on DomainException catch (e) {
-        emit(state.copyWith(
-          status: Erc20TokensStatus.error,
-          errorMessage: e.getReason(),
-        ));
-      } catch (e) {
-        emit(state.copyWith(
-          status: Erc20TokensStatus.error,
-          errorMessage: 'Error loading tokens',
-        ));
-      }
-    });
+    _currentNetworkSubscription = _networksRepository.watchCurrentNetwork().listen(
+      (event) {
+        add(Erc20TokensLoadRequested());
+      },
+    );
+    on<Erc20TokensLoadRequested>(_handleErc20TokensLoadRequested);
+  }
+
+  Future<void> _handleErc20TokensLoadRequested(Erc20TokensLoadRequested event, Emitter<Erc20TokensState> emit) async {
+    try {
+      emit(state.copyWith(status: Erc20TokensStatus.loading));
+      final network = await _networksRepository.getCurrentNetwork();
+      final tokens = await _erc20tokenRepository.getAllImportedTokensOfNetwork(network.id!);
+      final result = await _getErc20BalancesUsecase.execute();
+      final tokensWithBalances = tokens.map(
+        (e) {
+          return Erc20TokenWithBalance(
+            balance: result.balanceOf[e.address] ?? EtherAmount.fromString(value: '0'),
+            token: e,
+          );
+        },
+      ).toList();
+      emit(
+        state.copyWith(
+          status: Erc20TokensStatus.loaded,
+          tokens: tokensWithBalances,
+        ),
+      );
+    } on DomainException catch (e) {
+      emit(state.copyWith(
+        status: Erc20TokensStatus.error,
+        errorMessage: e.getReason(),
+      ));
+    } catch (e) {
+      emit(state.copyWith(
+        status: Erc20TokensStatus.error,
+        errorMessage: 'Error loading tokens',
+      ));
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _currentNetworkSubscription?.cancel();
+    return super.close();
   }
 }
--- lib/blocs/erc20_tokens/erc20_tokens_event.dart
+++ lib/blocs/erc20_tokens/erc20_tokens_event.dart
@@ -1,9 +1,6 @@
 part of 'erc20_tokens_bloc.dart';
 
-
 sealed class Erc20TokensEvent {}
 
-final class _Erc20TokensRefreshed extends Erc20TokensEvent {}
 
 final class Erc20TokensLoadRequested extends Erc20TokensEvent {}
-
