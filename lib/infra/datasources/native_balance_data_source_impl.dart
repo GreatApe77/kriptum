88be1e3f959e207662e40a0fe92a45bcd6101b90
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-07-29 13:38:59 -0300

    refactor: use web3 client abstraction

--- lib/infra/datasources/native_balance_data_source_impl.dart
+++ lib/infra/datasources/native_balance_data_source_impl.dart
@@ -1,21 +1,20 @@
 import 'package:kriptum/domain/models/ether_amount.dart';
 import 'package:kriptum/domain/models/network.dart';
-import 'package:http/http.dart' as http;
 import 'package:kriptum/infra/datasources/data_sources.dart';
-import 'package:web3dart/web3dart.dart' as web3;
+import 'package:kriptum/infra/network/web3_client.dart';
 
 class NativeBalanceDataSourceImpl implements NativeBalanceDataSource {
-  final http.Client _httpClient;
-  NativeBalanceDataSourceImpl(this._httpClient);
+  final Web3Client _web3client;
+  NativeBalanceDataSourceImpl(this._web3client);
   @override
   Future<EtherAmount> getNativeBalanceOfAccount({
     required String accountAddress,
     required Network network,
   }) async {
-    final web3Client = web3.Web3Client(network.rpcUrl, _httpClient);
-    final balance = await web3Client.getBalance(web3.EthereumAddress.fromHex(accountAddress));
-    return EtherAmount(
-      valueInWei: balance.getInWei,
+    final balance = await _web3client.getBalance(
+      address: accountAddress,
+      rpcUrl: network.rpcUrl,
     );
+    return EtherAmount(valueInWei: balance);
   }
 }
